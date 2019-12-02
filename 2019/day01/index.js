d148df62b8206275a6eab7b10ea9c5189427aa28
Thomas Prochazka <thomas.prochazka@gmail.com>
2019-12-02 20:15:13 +0100

    Complete 2019 Day 01

--- 2019/day01/index.js
+++ 2019/day01/index.js
@@ -1,4 +1,28 @@
+// Day 1: The Tyranny of the Rocket Equation
 const fs = require("fs");
 
-const content = fs.readFileSync("./input.txt");
-console.log(typeof content);
+const calculateFuel = (mass) => Math.floor(mass / 3) - 2;
+const prepareInput = () => fs.readFileSync("./input.txt")
+    .toString()
+    .split("\r\n")
+    .map(inp => parseInt(inp, 10));
+
+const calculateRequiredFuel = (input, calcFuelFn) => input.map(calcFuelFn).reduce((prev, curr) => prev + curr, 0);
+
+const input = prepareInput();
+const requiredFuel = calculateRequiredFuel(input, calculateFuel);
+console.log("Result (Part 1):", requiredFuel);
+
+// Part Two
+const calculateFuelV2 = (mass) => {
+    const fuel = Math.floor(mass / 3) - 2;
+
+    if (fuel <= 0) {
+        return 0;
+    }
+
+    return fuel + calculateFuelV2(fuel);
+}
+
+const requiredFuelV2 = calculateRequiredFuel(input, calculateFuelV2);
+console.log("Result (Part 2):", requiredFuelV2);
