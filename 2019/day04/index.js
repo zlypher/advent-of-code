6ffd764efc75206c58743a46241bdd8024d56b89
Thomas Prochazka <thomas.prochazka@gmail.com>
2019-12-07 11:55:03 +0100

    Add solution for 2019 Day 04 Part 2

--- 2019/day04/index.js
+++ 2019/day04/index.js
@@ -21,11 +21,11 @@ const isValidPassword = (password) => {
     return hasAdjacent;
 }
 
-const findPasswords = (minPw, maxPw) => {
+const findPasswords = (minPw, maxPw, isValidFn) => {
     const passwords = [];
 
     for (let curr = minPw; curr < maxPw; ++curr) {
-        if (isValidPassword(curr)) {
+        if (isValidFn(curr)) {
             passwords.push(curr);
         }
     }
@@ -33,5 +33,42 @@ const findPasswords = (minPw, maxPw) => {
     return passwords;
 }
 
-const passwords = findPasswords(min, max);
+const passwords = findPasswords(min, max, isValidPassword);
 console.log("Result (Part 1):", passwords.length);
+
+// Part Two
+
+const isValidPasswordV2 = (password) => {
+    const passwordArr = password.toString().split("").map(x => parseInt(x, 10));
+    let previous = passwordArr[0];
+    let hasDoubleDigit = false;
+    let digitCount = 1;
+
+    for (let idx = 1; idx < passwordArr.length; ++idx) {
+        const current = passwordArr[idx];
+        if (current < previous) {
+            return false;
+        }
+
+        if (current === previous) {
+            digitCount++;
+        } else {
+            if (digitCount === 2) {
+                hasDoubleDigit = true;
+            }
+
+            digitCount = 1;
+        }
+
+        previous = current;
+    }
+
+    if (digitCount === 2) {
+        hasDoubleDigit = true;
+    }
+
+    return hasDoubleDigit;
+}
+
+const passwordsV2 = findPasswords(min, max, isValidPasswordV2);
+console.log("Result (Part 2):", passwordsV2.length);
