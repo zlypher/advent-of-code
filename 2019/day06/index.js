bffc57d276b6ec8e246fae692fd171cfdf82d201
Thomas Prochazka <thomas.prochazka@gmail.com>
2019-12-08 01:09:55 +0100

    Add solution for 2019 Day 06

--- 2019/day06/index.js
+++ 2019/day06/index.js
@@ -2,10 +2,40 @@
 const fs = require("fs");
 
 let orbitData = fs.readFileSync("./input.txt").toString().split("\r\n");
-console.log(orbitData);
+
+const setupGraph = (orbitData) => {
+    let graph = orbitData.reduce((prev, curr) => {
+        let [from, to] = curr.split(")");
+
+        if (!prev.hasOwnProperty(from)) {
+            prev[from] = {
+                children: []
+            };
+        }
+
+        prev[from].children.push(to);
+
+        return prev;
+    }, {});
+
+    return graph;
+}
+
+const countOrbits = (graph, nodes, depth = 1) => {
+    const childOrbits = nodes.reduce((prev, curr) => {
+        if (!graph[curr]) {
+            return prev + depth;
+        }
+
+        return prev + countOrbits(graph, graph[curr].children, depth + 1);
+    }, 0);
+
+    return depth - 1 + childOrbits;
+}
 
 const findTotalOrbits = (orbitData) => {
-    return 0;
+    const graph = setupGraph(orbitData);
+    return countOrbits(graph, graph["COM"].children);
 };
 
 const totalOrbits = findTotalOrbits(orbitData);
