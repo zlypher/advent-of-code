8635363aa6188f69029ab696af9704f024c08877
Thomas Prochazka <thomas.prochazka@gmail.com>
2019-12-08 23:31:57 +0100

    Add placeholder for logic for 2019 Day 07 Part 2

--- 2019/day07/index.js
+++ 2019/day07/index.js
@@ -34,15 +34,15 @@ const testWithPhaseSetting = (program, phaseSetting) => {
     return output;
 }
 
-const amplifierTester = (program) => {
+const amplifierTester = (program, phaseSettings, testerFn) => {
     let highestSignal = 0;
 
     // Get all permutations of phase settings
-    const phaseSettingPermutations = permutate([0, 1, 2, 3, 4]);
+    const phaseSettingPermutations = permutate(phaseSettings);
 
     // For each permutation, run simulation
     for (let i = 0; i < phaseSettingPermutations.length; ++i) {
-        const signal = testWithPhaseSetting([...program], phaseSettingPermutations[i]);
+        const signal = testerFn([...program], phaseSettingPermutations[i]);
         if (signal > highestSignal) {
             console.log("Found higher signal", signal, "with", phaseSettingPermutations[i]);
             highestSignal = signal;
@@ -52,5 +52,25 @@ const amplifierTester = (program) => {
     return highestSignal;
 }
 
-const result = amplifierTester(program);
-console.log("Result (Part 1):", result);
+// const result = amplifierTester(program, [0, 1, 2, 3, 4], testWithPhaseSetting);
+// console.log("Result (Part 1):", result);
+
+// Part 2
+
+const testWithPhaseSettingV2 = (program, phaseSetting) => {
+    let output = 0;
+
+    for (let i = 0; i < phaseSetting.length; ++i) {
+        const setting = phaseSetting[i];
+        output = runProgram([...program], [setting, output]);
+    }
+
+    return output;
+}
+
+
+program = [3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5];
+
+// TODO
+// const resultV2 = amplifierTester(program, [5, 6, 7, 8, 9], testWithPhaseSettingV2);
+// console.log("Result (Part 2):", resultV2);
