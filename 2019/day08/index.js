84b0ba4669b6310f13fe2f81de0a327cb2c776d6
Thomas Prochazka <thomas.prochazka@gmail.com>
2019-12-09 16:15:06 +0100

    Add solution and update description for 2019 Day 08 Part 2

--- 2019/day08/index.js
+++ 2019/day08/index.js
@@ -45,3 +45,43 @@ const doSomething = (data, w, h) => {
 
 const result = doSomething(imageData, width, height);
 console.log("Result (Part 1):", result);
+
+// Part 2
+
+const PIX_BLACK = 0;
+const PIX_WHITE = 1;
+const PIX_TRANS = 2;
+
+const prepareImage = (data, w, h) => {
+    const finalImage = new Array(w * h).fill(PIX_TRANS);
+    const layers = prepareLayers(data, w, h);
+
+    for (let i = 0; i < layers.length; ++i) {
+        for (let j = 0; j < layers[i].length; ++j) {
+            if (finalImage[j] === PIX_TRANS) {
+                finalImage[j] = layers[i][j];
+            }
+        }
+    }
+
+    return finalImage;
+};
+
+const drawResult = (result, w) => {
+    for (let i = 0; i < result.length; ++i) {
+        if (i !== 0 && i % w === 0) {
+            process.stdout.write("\n");
+        }
+
+        switch (result[i]) {
+            case PIX_BLACK: process.stdout.write("0"); break;
+            case PIX_WHITE: process.stdout.write("1"); break;
+            case PIX_TRANS: process.stdout.write(" "); break;
+            default: break;
+        }
+    }
+};
+
+const resultV2 = prepareImage(imageData, width, height);
+console.log("Result (Part 2):");
+drawResult(resultV2, width);
