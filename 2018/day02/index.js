da8e0f9158be12ca296be15b325eaf9012bbf28e
Thomas Prochazka <thomas.prochazka@gmail.com>
2019-12-10 21:11:06 +0100

    Add solution for 2018 Day 02

--- 2018/day02/index.js
+++ 2018/day02/index.js
@@ -2,11 +2,71 @@
 const fs = require("fs");
 let boxIds = fs.readFileSync("./input.txt").toString().split("\r\n");
 
-console.log(boxIds);
+const getCounts = (id) => {
+    let counts = id.split("")
+        .reduce((prev, curr) => {
+            if (!prev[curr]) {
+                prev[curr] = 0;
+            }
+
+            prev[curr] += 1;
+            return prev;
+        }, {});
+
+    return {
+        hasTwos: Object.values(counts).indexOf(2) !== -1,
+        hasThrees: Object.values(counts).indexOf(3) !== -1
+    };
+};
 
 const findChecksum = (boxIds) => {
-    return 0;
+    let numTwos = 0;
+    let numThrees = 0;
+
+    for (let i = 0; i < boxIds.length; ++i) {
+        const res = getCounts(boxIds[i]);
+
+        numTwos = numTwos + (res.hasTwos ? 1 : 0);
+        numThrees = numThrees + (res.hasThrees ? 1 : 0);
+    }
+
+    return numTwos * numThrees;
 };
 
 const checksum = findChecksum(boxIds);
 console.log("Result (Part 1):", checksum);
+
+// Part 2
+
+const findCommonPart = (boxIds) => {
+    for (let i = 0; i < boxIds.length; ++i) {
+        const firstId = boxIds[i];
+        for (let j = 0; j < boxIds.length; ++j) {
+            if (i === j) {
+                continue;
+            }
+
+            const secondId = boxIds[j];
+            let numDiff = 0;
+            let diffIndex = -1;
+            for (let k = 0; k < secondId.length; ++k) {
+                if (firstId[k] !== secondId[k]) {
+                    numDiff++;
+                    diffIndex = k;
+                    if (numDiff > 1) {
+                        break;
+                    }
+                }
+            }
+
+            if (numDiff === 1) {
+                return firstId.substr(0, diffIndex) + firstId.substr(diffIndex + 1);
+            }
+        }
+    }
+
+    return false;
+};
+
+const commonIdPart = findCommonPart(boxIds);
+console.log("Result (Part 2):", commonIdPart);
