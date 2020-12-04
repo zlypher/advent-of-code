17571547e9a17144302eb5e76fb9e299fd04f19d
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-12-04 09:12:52 +0100

    Add solution for 2020 Day 3 Part 2

--- 2020/day03/index.js
+++ 2020/day03/index.js
@@ -1,4 +1,4 @@
-// Day 2: Password Philosophy
+// Day 3: Toboggan Trajectory
 const fs = require("fs");
 
 const prepareInput = () => fs.readFileSync("./input.txt")
@@ -12,29 +12,44 @@ function solvePartOne(input) {
     const sizeHorizontal = input[0].length;
     const sizeVertical = input.length;
 
-    console.log({ sizeHorizontal, sizeVertical });
-    const stepRight = 3;
-    const stepDown = 1;
+    return findTreesForSlope(input, sizeHorizontal, sizeVertical, 3, 1);
+}
+
+const findTreesForSlope = (input, sizeH, sizeV, stepR, stepD) => {
     let countTrees = 0;
     let posHorizontal = 0;
     let posVertical = 0;
 
-    console.log(input[posVertical][posHorizontal]);
-
-    while (posVertical < sizeVertical) {
+    while (posVertical < sizeV) {
         const field = input[posVertical][posHorizontal];
-        // console.log(field);
 
         if (field === FIELD_TREE) {
             countTrees += 1;
         }
 
-        posHorizontal = (posHorizontal + stepRight) % sizeHorizontal;
-        posVertical = posVertical + stepDown;
+        posHorizontal = (posHorizontal + stepR) % sizeH;
+        posVertical = posVertical + stepD;
     }
 
     return countTrees;
 }
 
+function solvePartTwo(input) {
+    const sizeHorizontal = input[0].length;
+    const sizeVertical = input.length;
+    const slopes = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2]
+    ];
+
+    return slopes
+        .map(([r, d]) => findTreesForSlope(input, sizeHorizontal, sizeVertical, r, d))
+        .reduce((prev, curr) => { return prev * curr; }, 1);
+}
+
 const input = prepareInput();
 console.log(solvePartOne(input));
+console.log(solvePartTwo(input));
