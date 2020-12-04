be0b5aad1dbdb226007bcdab72d32485a269bc88
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-12-04 10:58:26 +0100

    Add solution for 2020 Day 4 Part 2

--- 2020/day04/index.js
+++ 2020/day04/index.js
@@ -7,12 +7,37 @@ const prepareInput = () => fs.readFileSync("./input.txt")
 
 const fields = [ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]; // , "cid"
 
+const validEyeColors = [ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" ];
+const validateYear = (min, max) => (val) => {
+    const numYear = parseInt(val, 10);
+    return val.length === 4 && min <= numYear && numYear <= max;
+}
+const validateHeight = (val) => {
+    const num = val.substr(0, val.length - 2);
+    const sys = val.substr(-2);
+    if (sys !== "cm" && sys !== "in") {
+        return false;
+    }
+
+    return sys === "cm"
+        ? 150 <= num && num <= 193
+        : 59 <= num && num <= 76
+}
+
+const validators = {
+    "byr": validateYear(1920, 2002),
+    "iyr": validateYear(2010, 2020),
+    "eyr": validateYear(2020, 2030),
+    "hgt": validateHeight,
+    "hcl": (val) => /^#[0-9a-f]{6}$/.test(val),
+    "ecl": (val) => validEyeColors.find(col => col === val),
+    "pid": (val) => /^\d{9}$/.test(val),
+}
+
 function parsePassports(input) {
     const passports = [];
 
     let current = {};
-
-    // console.log(input);
 
     for (let i = 0; i < input.length; ++i) {
         const line = input[i];
@@ -28,11 +53,14 @@ function parsePassports(input) {
             .forEach(([name, val]) => current[name] = val);
     }
 
+    // Push the last one
+    passports.push(current);
+
 
     return passports;
 }
 
-function solvePartOne(passports) {
+function solvePartOneOrTwo(passports, checkPassport) {
     let countValidPassports = 0;
 
     for (let i = 0; i < passports.length; ++i) {
@@ -48,6 +76,11 @@ function checkPassport(passport) {
     return fields.every(field => passport.hasOwnProperty(field));
 }
 
+function checkPassportStrict(passport) {
+    return fields.every(field => passport.hasOwnProperty(field) && validators[field](passport[field]));
+}
+
 const input = prepareInput();
 const passports = parsePassports(input);
-console.log(solvePartOne(passports));
+console.log(solvePartOneOrTwo(passports, checkPassport));
+console.log(solvePartOneOrTwo(passports, checkPassportStrict));
