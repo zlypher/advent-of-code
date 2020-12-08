9da0258b02084f2e87211dfb485c928d33a71034
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-12-08 21:13:54 +0100

    Prepare folder for 2020 Day 8

---
+++ 2020/day08/index.js
@@ -0,0 +1,14 @@
+// Day 8: Handheld Halting
+const fs = require("fs");
+
+const prepareInput = () => fs.readFileSync("./input.txt")
+    .toString()
+    .split("\r\n");
+
+function solvePartOne(input) {
+
+    return 0;
+}
+
+const input = prepareInput();
+console.log(solvePartOne(input));
