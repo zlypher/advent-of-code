5fccbbe85bcccbdbdec850d806ec826a55e5632b
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-12-10 17:12:09 +0100

    Add solution for 2020 Day 8 Part 2

--- 2020/day08/index.js
+++ 2020/day08/index.js
@@ -1,5 +1,6 @@
 // Day 8: Handheld Halting
 const fs = require("fs");
+const { start } = require("repl");
 
 const OP_NOP = "nop";
 const OP_ACC = "acc";
@@ -14,11 +15,13 @@ const gameConsole = (instructions) => {
     const globals = {
         accumulator: 0,
         pointer: 0,
+        len: instructions.length,
     }
 
     return {
         acc: () => globals.accumulator,
         ptr: () => globals.pointer,
+        running: () => globals.pointer < globals.len,
         runNext: () => {
             const [op, arg] = parseInstruction(instructions[globals.pointer]);
 
@@ -61,5 +64,67 @@ function solvePartOne(instructions) {
     return acc;
 }
 
+function findInstructionToPatch(instructions, startPatchPtr) {
+    return instructions.findIndex((ins, idx) => {
+        return idx >= startPatchPtr && (ins.startsWith(OP_NOP) || ins.startsWith(OP_JMP));
+    });
+}
+
+function swapInstruction(instructions, ptr, firstOp, secondOp) {
+    const instruction = instructions[ptr];
+    const op = instruction.substr(0, 3);
+    const arg = instruction.substr(3);
+    const newOp = op === firstOp ? secondOp : firstOp;
+    instructions[ptr] = newOp + arg;
+}
+
+function patchInstructions(instructions, startPatchPtr) {
+    const newInstructions = [...instructions];
+    let patchPtr = findInstructionToPatch(newInstructions, startPatchPtr);
+
+    swapInstruction(newInstructions, patchPtr, OP_NOP, OP_JMP);
+
+    return [newInstructions, patchPtr];
+}
+
+function debugProgram(instructions) {
+    const hgc = gameConsole(instructions);
+
+    const seenInstructions = {};
+    let current = 0;
+    let loopDetected = false;
+    let running = true;
+
+    while (!loopDetected && running) {
+        hgc.runNext();
+
+        seenInstructions[current] = true;
+        current = hgc.ptr();
+
+        loopDetected = seenInstructions.hasOwnProperty(current);
+        running = hgc.running();
+    }
+
+    const result = loopDetected ? -1 : 0;
+
+    return [result, hgc.acc()];
+}
+
+function solvePartTwo(instructions) {
+    const lenInstructions = instructions.length;
+    let patchPtr = 0;
+
+    while (patchPtr < lenInstructions) {
+        const [patchedInstructions, foundPatchPtr] = patchInstructions(instructions, patchPtr);
+        patchPtr = foundPatchPtr + 1;
+
+        const [result, acc] = debugProgram(patchedInstructions);
+        if (result === 0) {
+            return acc;
+        }
+    }
+}
+
 const input = prepareInput();
-console.log(solvePartOne(input));
+// console.log(solvePartOne(input));
+console.log(solvePartTwo(input));
