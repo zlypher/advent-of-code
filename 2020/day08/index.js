bbb746eb9a89748b321ccd28d24619bafe304310
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-12-10 15:48:44 +0100

    Add solution for 2020 Day 8 Part 1

--- 2020/day08/index.js
+++ 2020/day08/index.js
@@ -1,13 +1,64 @@
 // Day 8: Handheld Halting
 const fs = require("fs");
 
+const OP_NOP = "nop";
+const OP_ACC = "acc";
+const OP_JMP = "jmp";
+
+const parseInstruction = (instruction) => {
+    const [fst, snd] = instruction.split(" ");
+    return [fst, parseInt(snd, 10)];
+}
+
+const gameConsole = (instructions) => {
+    const globals = {
+        accumulator: 0,
+        pointer: 0,
+    }
+
+    return {
+        acc: () => globals.accumulator,
+        ptr: () => globals.pointer,
+        runNext: () => {
+            const [op, arg] = parseInstruction(instructions[globals.pointer]);
+
+            switch (op) {
+                case OP_ACC:
+                    globals.accumulator += arg;
+                    globals.pointer += 1;
+                    break;
+                case OP_JMP:
+                    globals.pointer += arg;
+                    break;
+                case OP_NOP:
+                default:
+                    globals.pointer += 1;
+                    break;
+            }
+        }
+    };
+}
+
 const prepareInput = () => fs.readFileSync("./input.txt")
     .toString()
     .split("\r\n");
 
-function solvePartOne(input) {
+function solvePartOne(instructions) {
+    const hgc = gameConsole(instructions);
 
-    return 0;
+    const seenInstructions = {};
+    let current = 0;
+    let acc = 0;
+
+    while (!seenInstructions.hasOwnProperty(current)) {
+        hgc.runNext();
+
+        seenInstructions[current] = true;
+        current = hgc.ptr();
+        acc = hgc.acc();
+    }
+
+    return acc;
 }
 
 const input = prepareInput();
