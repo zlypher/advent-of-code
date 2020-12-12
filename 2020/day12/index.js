9328ed0f08fed9c7f3217eecb6a67ee8a3878c79
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-12-12 18:23:29 +0100

    Add solution for 2020 Day 12 Part 2

--- 2020/day12/index.js
+++ 2020/day12/index.js
@@ -45,5 +45,71 @@ function solvePartOne(input) {
     return Math.abs(moveMap["N"] - moveMap["S"]) + Math.abs(moveMap["E"] - moveMap["W"]);
 }
 
+const moveLeft = (wpMap, num) => {
+    for (let i = 0; i < num; ++i) {
+        let prevNS = wpMap["N"];
+        let prevEW = wpMap["E"];
+
+        wpMap["N"] = prevEW;
+        wpMap["E"] = prevNS * -1;
+    }
+}
+
+const moveRight = (wpMap, num) => {
+    for (let i = 0; i < num; ++i) {
+        prevNS = wpMap["N"];
+        prevEW = wpMap["E"];
+
+        wpMap["N"] = prevEW * -1;
+        wpMap["E"] = prevNS;
+    }
+}
+
+function solvePartTwo(input) {
+    const inverseSW = (dir) => dir === "S" ? "N" : "E";
+    const posMap = {
+        "N": 0,
+        "E": 0,
+    };
+
+    const wpMap = {
+        "N": 1,
+        "E": 10,
+    }
+
+    for (let i = 0; i < input.length; ++i) {
+        const line = input[i];
+        const cmd = line[0];
+        const val = parseInt(line.substr(1, line.length - 1), 10);
+
+        switch (cmd) {
+            case "F":
+                posMap["N"] += wpMap["N"] * val;
+                posMap["E"] += wpMap["E"] * val;
+                break;
+            case "N":
+            case "E":
+                wpMap[cmd] += val;
+                break;
+            case "S":
+            case "W":
+                wpMap[inverseSW(cmd)] -= val;
+                break;
+            case "L":
+                moveLeft(wpMap, val / 90);
+                break;
+            case "R":
+                moveRight(wpMap, val / 90);
+                break;
+            default:
+                console.log("INVALID");
+                break;
+        }
+    }
+
+    return Math.abs(posMap["N"]) + Math.abs(posMap["E"]);
+}
+
 const input = prepareInput();
 console.log(solvePartOne(input));
+console.log(solvePartTwo(input));
