178abf4a702c4a5f2970abee365eb67a073ad9bf
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-12-27 12:28:51 +0100

    Add solution for 2020 Day 22 Part 2

--- 2020/day22/index.js
+++ 2020/day22/index.js
@@ -5,7 +5,7 @@ const prepareInput = () => fs.readFileSync("./input.txt")
     .toString()
     .split("\r\n");
 
-function solvePartOne(data) {
+function solvePartOne(input) {
     const first = parsePlayerDeck(input, 0);
     const second = parsePlayerDeck(input, first.length + 2);
 
@@ -54,5 +54,67 @@ function calculateScore(deck) {
     }, 0);
 }
 
+function solvePartTwo(input) {
+    const first = parsePlayerDeck(input, 0);
+    const second = parsePlayerDeck(input, first.length + 2);
+
+    const [playerOneWon, deckOne, deckTwo] = playGamePartTwo(first, second);
+
+    return playerOneWon
+        ? calculateScore(deckOne)
+        : calculateScore(deckTwo);
+}
+
+function toKey(arr1, arr2) {
+    return [
+        arr1.join(","),
+        arr2.join(","),
+    ].join("-");
+}
+
+function playGamePartTwo(first, second) {
+    const alreadyPlayed = {};
+
+    const deckOne = [...first];
+    const deckTwo = [...second];
+
+    let round = 1;
+
+    while (deckOne.length > 0 && deckTwo.length > 0) {
+        // Check prev state
+        const playedKey = toKey(deckOne, deckTwo);
+        const res = alreadyPlayed[playedKey];
+
+        if (res) {
+            return [true, deckOne, deckTwo];
+        }
+
+        // Pick card
+        const fst = deckOne.shift();
+        const snd = deckTwo.shift();
+
+        // console.log({ round, fst, snd, deckOne, deckTwo})
+
+        let playSubGame = fst <= deckOne.length && snd <= deckTwo.length;
+        let [playerOneWon, ...rest] = playSubGame
+            ? playGamePartTwo(deckOne.slice(0, fst), deckTwo.slice(0, snd))
+            : [fst > snd, deckOne, deckTwo];
+
+        alreadyPlayed[playedKey] = true;
+
+        // Add to winner
+        if (playerOneWon) {
+            deckOne.push(fst, snd);
+        } else {
+            deckTwo.push(snd, fst);
+        }
+
+        round++;
+    }
+
+    return [deckOne.length > 0, deckOne, deckTwo];
+}
+
 const input = prepareInput();
-console.log(solvePartOne(input));
+// console.log(solvePartOne(input));
+console.log(solvePartTwo(input));
