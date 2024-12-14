71a9ce2e4b6a1f878233751202bfe3b42505dd75
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-12-14 12:44:35 +0100

    Add 2024 Day 14 (incomplete)

---
+++ 2024/day14/index.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+
+const prepareInput = () => {
+  const robots = [];
+  const lines = fs.readFileSync("./input.txt").toString().split("\r\n");
+
+  const extractRobot = (conf, regex) => {
+    const matches = [...conf.matchAll(regex)];
+    return {
+      pos: {
+        x: parseInt(matches[0][1], 10),
+        y: parseInt(matches[0][2], 10),
+      },
+      v: {
+        x: parseInt(matches[0][3], 10),
+        y: parseInt(matches[0][4], 10),
+      },
+    };
+  };
+
+  while (typeof (line = lines.shift()) !== "undefined") {
+    const regex = /p=(-?\d*),(-?\d*) v=(-?\d*),(-?\d*)/g;
+
+    robots.push(extractRobot(line, regex));
+  }
+
+  return { robots };
+};
+
+const input = prepareInput();
+
+function solvePartOne(input) {
+  const { robots } = input;
+
+  const seconds = 100;
+  const width = 101; // 101
+  const height = 103; // 103
+  const quadrants = [
+    [0, 0],
+    [0, 0],
+  ];
+
+  for (let robot of robots) {
+    const endPosition = {
+      x: (((robot.pos.x + robot.v.x * seconds) % width) + width) % width,
+      y: (((robot.pos.y + robot.v.y * seconds) % height) + height) % height,
+    };
+
+    const heightMid = Math.floor(height / 2);
+    const widthMid = Math.floor(width / 2);
+
+    if (endPosition.y === heightMid || endPosition.x === widthMid) continue;
+
+    let yIdx = endPosition.y < heightMid ? 0 : 1;
+    let xIdx = endPosition.x < widthMid ? 0 : 1;
+
+    quadrants[yIdx][xIdx] += 1;
+  }
+
+  return quadrants.reduce((prev, curr) => {
+    return prev * curr.reduce((p, c) => p * c, 1);
+  }, 1);
+}
+
+console.log(solvePartOne(input));
+
+function solvePartTwo(input) {}
+
+console.log(solvePartTwo(input));
