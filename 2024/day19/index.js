b7087a42c7b9d128e583ced42ae4c630e5285a36
Thomas Prochazka <thomas.prochazka@gmail.com>
2024-12-23 23:04:37 +0100

    Add 2024 Day 19 (incomplete)

---
+++ 2024/day19/index.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+
+const prepareInput = () => {
+  let patterns = [];
+  const designs = [];
+
+  const lines = fs.readFileSync("./input.txt").toString().split("\r\n");
+
+  patterns = lines.shift().split(", ");
+  lines.shift();
+  while (typeof (line = lines.shift()) !== "undefined") {
+    designs.push(line);
+  }
+
+  return { patterns, designs };
+};
+
+const input = prepareInput();
+
+function solvePartOne(input) {
+  const { patterns, designs } = input;
+
+  let count = 0;
+
+  for (let design of designs) {
+    if (checkDesign(design, patterns)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(solvePartOne(input));
+
+function solvePartTwo(input) {
+  const { patterns, designs } = input;
+
+  let count = 0;
+
+  for (let design of designs) {
+    console.log("check design", design);
+    count += checkDesignV2(design, patterns);
+  }
+
+  return count;
+}
+
+console.log(solvePartTwo(input));
+
+function checkDesign(design, patterns) {
+  if (design.length === 0) return true;
+
+  let match = false;
+
+  for (let pattern of patterns) {
+    if (design.startsWith(pattern)) {
+      match = match || checkDesign(design.substring(pattern.length), patterns);
+    }
+  }
+
+  return match;
+}
+
+function checkDesignV2(design, patterns) {
+  if (design.length === 0) return 1;
+
+  let count = 0;
+
+  for (let pattern of patterns) {
+    if (design.startsWith(pattern)) {
+      count += checkDesignV2(design.substring(pattern.length), patterns);
+    }
+  }
+
+  return count;
+}
