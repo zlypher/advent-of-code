496c4540b8cea23902d0bf5903d3c14db6683b54
Thomas Prochazka <thomas.prochazka@gmail.com>
2025-12-21 20:29:18 +0100

    start to setup npm with ts and playholder day

---
+++ 2025/day-placeholder/logic.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, it } from "vitest";
+import { prepareInput, solvePartOne, solvePartTwo } from "./logic";
+
+describe("Day X", () => {
+  describe("Part One", () => {
+    it("should solve the example input", () => {
+      const rawInput = `placeholder`;
+      const input = prepareInput(rawInput);
+      const result = solvePartOne(input);
+
+      expect(result).toBe(-1);
+    });
+
+    it("should solve the actual input", () => {
+      const rawInput = `placeholder`;
+      const input = prepareInput(rawInput);
+      const result = solvePartOne(input);
+
+      expect(result).toBe(-1);
+    });
+  });
+  describe("Part Two", () => {
+    it("should solve the example input", () => {
+      const rawInput = `placeholder`;
+      const input = prepareInput(rawInput);
+      const result = solvePartTwo(input);
+
+      expect(result).toBe(-1);
+    });
+
+    it("should solve the actual input", () => {
+      const rawInput = `placeholder`;
+      const input = prepareInput(rawInput);
+      const result = solvePartTwo(input);
+
+      expect(result).toBe(-1);
+    });
+  });
+});
