2387f712d0c7456a7c4defbb14a83e76a3f1fb07
Thomas Prochazka <thomas.prochazka@gmail.com>
2025-12-21 21:21:58 +0100

    feat: complete 2025 day 01

--- 2025/day01/logic.test.ts
+++ 2025/day01/logic.test.ts
@@ -1,5 +1,7 @@
+import * as fs from "node:fs";
+import path from "node:path";
 import { describe, expect, it } from "vitest";
-import { prepareInput, solvePartOne } from "./logic";
+import { prepareInput, solvePartOne, solvePartTwo } from "./logic";
 
 describe("Day 1", () => {
   describe("Part One", () => {
@@ -18,6 +20,45 @@ L82`;
       const result = solvePartOne(input);
 
       expect(result).toBe(3);
+    });
+
+    it("should solve the actual input", () => {
+      const rawInput = fs
+        .readFileSync(path.resolve(import.meta.dirname, "input.txt"))
+        .toString();
+      const input = prepareInput(rawInput);
+      const result = solvePartOne(input);
+
+      expect(result).toBe(1102);
+    });
+  });
+
+  describe("Part Two", () => {
+    it("should solve the example input", () => {
+      const rawInput = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`;
+      const input = prepareInput(rawInput);
+      const result = solvePartTwo(input);
+
+      expect(result).toBe(6);
+    });
+
+    it("should solve the actual input", () => {
+      const rawInput = fs
+        .readFileSync(path.resolve(import.meta.dirname, "input.txt"))
+        .toString();
+      const input = prepareInput(rawInput);
+      const result = solvePartTwo(input);
+
+      expect(result).toBe(6175);
     });
   });
 });
