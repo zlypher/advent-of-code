5302dd353b468efb225e2d66ccc22992b4365fc4
Thomas Prochazka <thomas.prochazka@gmail.com>
2025-12-22 00:08:17 +0100

    feat: complete 2025 day 03

--- 2025/day03/logic.test.ts
+++ 2025/day03/logic.test.ts
@@ -3,43 +3,49 @@ import path from "node:path";
 import { describe, expect, it } from "vitest";
 import { prepareInput, solvePartOne, solvePartTwo } from "./logic";
 
-describe("Day X", () => {
+describe("Day 3", () => {
   describe("Part One", () => {
     it("should solve the example input", () => {
-      const rawInput = `placeholder`;
+      const rawInput = `987654321111111
+811111111111119
+234234234234278
+818181911112111`;
       const input = prepareInput(rawInput);
       const result = solvePartOne(input);
 
-      expect(result).toBe(-1);
+      expect(result).toBe(357);
     });
 
-    it.todo("should solve the actual input", () => {
+    it("should solve the actual input", () => {
       const rawInput = fs
         .readFileSync(path.resolve(import.meta.dirname, "input.txt"))
         .toString();
       const input = prepareInput(rawInput);
       const result = solvePartOne(input);
 
-      expect(result).toBe(-1);
+      expect(result).toBe(17535);
     });
   });
   describe("Part Two", () => {
-    it.todo("should solve the example input", () => {
-      const rawInput = `placeholder`;
+    it("should solve the example input", () => {
+      const rawInput = `987654321111111
+811111111111119
+234234234234278
+818181911112111`;
       const input = prepareInput(rawInput);
       const result = solvePartTwo(input);
 
-      expect(result).toBe(-1);
+      expect(result).toBe(3121910778619);
     });
 
-    it.todo("should solve the actual input", () => {
+    it("should solve the actual input", () => {
       const rawInput = fs
         .readFileSync(path.resolve(import.meta.dirname, "input.txt"))
         .toString();
       const input = prepareInput(rawInput);
       const result = solvePartTwo(input);
 
-      expect(result).toBe(-1);
+      expect(result).toBe(173577199527257);
     });
   });
 });
